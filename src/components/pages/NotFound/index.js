ba07002e59428b01a235ae1d7967d8d6c020b229
Andreas Tsika <andreastsika@gmail.com>
2023-08-13 00:44:48 +0300

    final project commit

---
+++ src/components/pages/NotFound/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const NotFound = () => {
+	return (
+		<div className="container page-not-found">
+			<h2>Page not found</h2>
+			<div className="page-not-found-code">- 404 -</div>
+		</div>
+	);
+};
+
+export default NotFound;
